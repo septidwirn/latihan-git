c64d8ec17842d16ed5762a2b935ca72381bef9b5
septidwirn <47975074+septidwirn@users.noreply.github.com>
2023-11-25 16:42:49 +0700

    Update Person.java
    Mengubah access modifier private menjadi default

--- Person.java
+++ Person.java
@@ -1,9 +1,6 @@
-package parents;
-
-
 public class Person {
-	private String name;
-	private String address;
+	String name;
+	String address;
 	
 	public Person(String name, String address) {
 		super();
@@ -18,20 +15,5 @@ public class Person {
 	public void greeting() {
 		System.out.println("Hello my name is " + name + ".");
 		System.out.println("I come from " + address + ".");
-	}
-	
-	//membuat getter dan setter
-	public String getName() {
-		return this.name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getAddress() {
-		return this.address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
 	}
 }
